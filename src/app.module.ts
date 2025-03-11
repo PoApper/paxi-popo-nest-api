334d196bbce37dba7a669e77cdb8a63daf2ca630
hegelty <skxodid0305@gmail.com>
2025-03-11 21:44:00 +0900

    Feat: Creat group entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { GroupModule } from './group/group.module';
 import configurations from './config/configurations';
 
 @Module({
@@ -26,6 +27,7 @@ import configurations from './config/configurations';
     }),
     AuthModule,
     UserModule,
+    GroupModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/group/entities/group.entity.ts
@@ -0,0 +1,45 @@
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+import { User } from 'src/user/entities/user.entity';
+import { GroupStatus } from 'src/group/entities/group.meta';
+
+@Entity()
+export class Group {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column({ nullable: false })
+  title: string;
+
+  @Column({ nullable: false })
+  @OneToOne(() => User, (user) => user.uuid)
+  ownerUuid: string;
+
+  @Column({ nullable: false })
+  departure: string;
+
+  @Column({ nullable: false })
+  destination: string;
+
+  @Column({ nullable: false, default: 4 })
+  maxParticipant: number;
+
+  @Column({ nullable: false, default: 1 })
+  currentParticipant: number;
+
+  @Column({ nullable: false })
+  departureTime: Date;
+
+  @Column({ nullable: false, default: GroupStatus.ACTIVATED })
+  status: GroupStatus;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column({ nullable: true })
+  @OneToOne(() => User, (user) => user.uuid)
+  payerUuid: string;
+
+  @Column({ nullable: true })
+  payAmount: number;
+}
