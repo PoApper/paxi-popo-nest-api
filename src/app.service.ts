c821d79191c8bfeeb37623c010d84ad7711fd13c
Gwanho Kim <khkim6040@gmail.com>
2025-03-11 17:36:40 +0900

    Chore: Add swagger document

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import * as moment from 'moment';
+import * as momentTz from 'moment-timezone';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Hello Paxi! (paxi-${
+      process.env.PAXI_VERSION || 'Local'
+    }) (server now: ${moment().format(
+      'YYYY-MM-DD HH:mm:ss',
+    )}, KST now: ${momentTz().tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss')})`;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Paxi API')
+    .setDescription('Paxi API documentation')
+    .setVersion('1.0')
+    .addTag('paxi')
+    .addBearerAuth()
+    .addCookieAuth('Authentication')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
   await app.listen(process.env.PORT || 4001);
 }
 
