2592e5dcd128331f385a60c90c6c6dd688dda17c
Gwanho Kim <khkim6040@gmail.com>
2025-03-23 19:30:57 +0900

    Fix: Entities path

--- src/config/configurations.ts
+++ src/config/configurations.ts
@@ -11,7 +11,7 @@ export default () => {
       username: isTest ? undefined : process.env.DATABASE_USERNAME,
       password: isTest ? undefined : process.env.DATABASE_PASSWORD,
       database: isTest ? ':memory:' : process.env.DATABASE_DATABASE,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: isTest ? true : process.env.DATABASE_SYNC === 'true', // 테스트 환경에서는 항상 true
       dropSchema: isTest, // ✅ 테스트 시 DB 초기화
     },
