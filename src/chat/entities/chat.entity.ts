ac50f9f08362ec71cc46b40d0f3e81e869e2e6a9
Gwanho Kim <khkim6040@gmail.com>
2025-03-23 19:25:11 +0900

    Fix: Entity error by deleting webpack and declaring type of senderUuid

--- src/chat/entities/chat.entity.ts
+++ src/chat/entities/chat.entity.ts
@@ -16,7 +16,12 @@ export class Chat {
   groupUuid: string;
 
   // 시스템 메시지인 경우 null(ex. 출발 시간 안내, 입퇴장 안내 메세지 등)
-  @Column({ nullable: true, collation: 'utf8mb4_general_ci' })
+  @Column({
+    type: 'varchar',
+    length: 36,
+    nullable: true,
+    collation: 'utf8mb4_general_ci',
+  })
   senderUuid: string | null;
 
   @Column({ nullable: false, type: 'text' })
