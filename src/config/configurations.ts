383757552df9b2c1eb47bae6093e496b15bd9f79
Gwanho Kim <khkim6040@gmail.com>
2025-03-31 15:01:08 +0900

    Test/make testing (#11)
    * Chore: Delete unused test files
    
    * Fix: Enable testing
    
    * Fix: Enable testing and introduce user service features to make fake user

--- src/config/configurations.ts
+++ src/config/configurations.ts
@@ -11,9 +11,12 @@ export default () => {
       username: isTest ? undefined : process.env.DATABASE_USERNAME,
       password: isTest ? undefined : process.env.DATABASE_PASSWORD,
       database: isTest ? ':memory:' : process.env.DATABASE_DATABASE,
-      entities: ['dist/**/*.entity{.ts,.js}'],
+      entities: isTest
+        ? ['src/**/*.entity{.ts,.js}']
+        : ['dist/**/*.entity{.ts,.js}'],
       synchronize: isTest ? true : process.env.DATABASE_SYNC === 'true', // 테스트 환경에서는 항상 true
       dropSchema: isTest, // ✅ 테스트 시 DB 초기화
+      charset: isTest ? undefined : 'utf8mb4',
     },
   };
 };
