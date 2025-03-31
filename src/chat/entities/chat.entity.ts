8ba6b4ced619be5a61373f4e7ac64dd1f179af9e
Gwanho Kim <khkim6040@gmail.com>
2025-03-31 21:58:15 +0900

    Feat: Use both id and uuid for cursor based pagination

--- src/chat/entities/chat.entity.ts
+++ src/chat/entities/chat.entity.ts
@@ -2,14 +2,23 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
 import { ChatMessageType } from 'src/chat/entities/chat.meta';
-
+import { Group } from 'src/group/entities/group.entity';
+import { User } from 'src/user/entities/user.entity';
+// 커서 기반 채팅 데이터 조회를 위한 복합 인덱스 생성
+@Index(['groupUuid', 'createdAt', 'id'])
 @Entity()
 export class Chat {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'uuid', unique: true, length: 36 })
   uuid: string;
 
   @Column({ nullable: false })
@@ -22,9 +31,31 @@ export class Chat {
   @Column({ nullable: false, type: 'text' })
   message: string;
 
-  @Column({ nullable: false })
+  @Column({
+    nullable: true,
+    default: ChatMessageType.TEXT,
+    type: 'enum',
+    enum: ChatMessageType,
+  })
   messageType: ChatMessageType;
 
   @CreateDateColumn()
   createdAt: Date;
+
+  /**
+   * Database Relation
+   */
+
+  @ManyToOne(() => Group, (group) => group.chats, {
+    onDelete: 'CASCADE',
+  })
+  // TODO: group uuid 크기 255바이트인데 36바이트로 제한해야 함
+  @JoinColumn({ name: 'groupUuid' })
+  group: Group;
+
+  @ManyToOne(() => User, (user) => user.chats, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'senderUuid' })
+  sender: User;
 }
