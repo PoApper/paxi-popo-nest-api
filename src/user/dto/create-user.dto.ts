ee58fd5b331c33e4a496c14b9bf7cafe147b6f1c
Gwanho Kim <khkim6040@gmail.com>
2025-03-31 15:01:08 +0900

    Test/make testing (#11)
    * Chore: Delete unused test files
    
    * Fix: Enable testing
    
    * Fix: Enable testing and introduce user service features to make fake user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { UserStatus, UserType } from '../user.meta';
+
+export class CreateUserDto {
+  readonly email: string;
+  readonly password: string;
+  readonly name: string;
+  readonly userType: UserType;
+}
+
+export class UpdateUserDto {
+  readonly email: string;
+  readonly name: string;
+  readonly userType: UserType;
+  readonly userStatus: UserStatus;
+}
+
+export class UpdatePasswordDto {
+  readonly password: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,35 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as crypto from 'crypto';
 
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserStatus } from './user.meta';
 
+// 해당 UserService는 테스트 환경에서 가짜 유저를 만들어내기 위해서만 사용되며, 실제 환경에서는 사용되지 않음
+// 실제 환경에서 Paxi는 유저 정보를 읽기만 함
+// 실제 환경에서 사용되지 않기 때문에 controller 또한 없음
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
+    private readonly userRepo: Repository<User>,
   ) {}
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async save(dto: CreateUserDto) {
+    const cryptoSalt = crypto.randomBytes(64).toString('base64');
+    const encryptedPassword = this.encryptPassword(dto.password, cryptoSalt);
+
+    return this.userRepo.save({
+      email: dto.email,
+      password: encryptedPassword,
+      cryptoSalt: cryptoSalt,
+      name: dto.name,
+      userType: dto.userType,
+      // 테스트 환경에서 유저 상태 ACTIVATED로 설정
+      userStatus: UserStatus.activated,
+      lastLoginAt: new Date(),
+    });
   }
 
-  findAll() {
-    return this.userRepository.find();
+  find(findOptions: object) {
+    return this.userRepo.find(findOptions);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  // password encrypt util
+  private encryptPassword(password: string, cryptoSalt: string) {
+    return crypto
+      .pbkdf2Sync(password, cryptoSalt, 10000, 64, 'sha512')
+      .toString('base64');
   }
 }
