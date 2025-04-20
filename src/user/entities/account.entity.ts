00b2b18cee50d516a88067c26df6d4a31f9e8ae1
Gwanho Kim <khkim6040@gmail.com>
2025-04-20 22:06:39 +0900

    Update: Account entity

--- src/user/entities/account.entity.ts
+++ src/user/entities/account.entity.ts
@@ -1,10 +1,12 @@
 import {
   BaseEntity,
   Column,
+  CreateDateColumn,
   Entity,
   JoinColumn,
   OneToOne,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 import { User } from './user.entity';
@@ -15,10 +17,16 @@ export class Account extends BaseEntity {
   id: number;
 
   @Column({ type: 'varchar', nullable: false })
-  accountNumber: string;
+  encryptedAccountNumber: string;
 
   @Column({ type: 'uuid', nullable: false })
   userUuid: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 
   @OneToOne(() => User, (user) => user.account, {
     onDelete: 'CASCADE',
