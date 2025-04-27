452fc9fc65a43d471a454dc5e44dd6e4f4680c66
Gwanho Kim <khkim6040@gmail.com>
2025-04-27 22:02:04 +0900

    Package: Install class-transformer for @Exclude annotation

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import {
@@ -7,6 +7,7 @@ import {
 } from 'firebase-admin/app';
 import { credential as firebaseCredential } from 'firebase-admin';
 import { ConfigService } from '@nestjs/config';
+import { ClassSerializerInterceptor } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
@@ -14,6 +15,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.use(cookieParser());
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Paxi API')
