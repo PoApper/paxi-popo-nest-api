ec127d925c2166e4e8328cadf1802c6bb0624d8d
Gwanho Kim <khkim6040@gmail.com>
2025-05-13 21:30:29 +0900

    Rename: Chat uuid name

--- src/chat/guards/chat-sender.guard.ts
+++ src/chat/guards/chat-sender.guard.ts
@@ -14,9 +14,9 @@ export class ChatSenderGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const userUuid = request.user.uuid;
-    const messageUuid = request.params.messageUuid;
+    const chatUuid = request.params.chatUuid;
 
-    const chat = await this.chatService.findOne(messageUuid);
+    const chat = await this.chatService.findOne(chatUuid);
     if (chat.senderUuid !== userUuid) {
       throw new ForbiddenException('메세지 전송자가 아닙니다.');
     }
