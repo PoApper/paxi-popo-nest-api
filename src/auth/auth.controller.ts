7f403c7d636877652c3cfddf99b85d2f0cc2cea2
hegelty <skxodid0305@gmail.com>
2025-08-12 01:34:41 +0900

    Feat: Add cookie handling logic for JWT tokens and update dependencies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Req, UnauthorizedException } from '@nestjs/common';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import { ApiCookieAuth } from '@nestjs/swagger';
+import ms from 'ms';
 
 import { UserType } from 'src/user/user.meta';
 import { GuardName } from 'src/common/guard-name';
 import { PublicGuard } from 'src/common/public-guard.decorator';
 
+import { jwtConstants } from './constants';
 import { AuthService } from './auth.service';
 import { JwtPayload } from './strategies/jwt.payload';
 
@@ -27,5 +29,36 @@ export class AuthController {
       }
     }
     return user;
+  }
+
+  private setCookies(
+    res: Response,
+    accessToken: string,
+    refreshToken: string,
+  ): void {
+    const domain =
+      process.env.NODE_ENV === 'prod'
+        ? 'popo.poapper.club'
+        : process.env.NODE_ENV === 'dev'
+          ? 'popo-dev.poapper.club'
+          : 'localhost';
+
+    res.cookie('Authentication', accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'local' ? false : true,
+      path: '/',
+      domain: domain,
+      sameSite: 'lax',
+      maxAge: ms(jwtConstants.refreshTokenExpirationTime),
+    });
+
+    res.cookie('Refresh', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'local' ? false : true,
+      path: '/auth/refresh',
+      domain: domain,
+      sameSite: 'lax',
+      maxAge: ms(jwtConstants.refreshTokenExpirationTime),
+    });
   }
 }
