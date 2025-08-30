97151d632b7a9d3889ca7df5490b5d5f80825c85
BlueHorn07 <bluehornblues@gmail.com>
2025-08-30 22:01:12 +0900

    check: pass roomuuid

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -122,6 +122,9 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
           usersToNotify,
           `${await this.roomService.getRoomTitle(roomUuid)}`,
           message.message,
+          {
+            roomUuid: roomUuid,
+          },
         )
         .catch(console.error);
     }
